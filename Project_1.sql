a7be97ba0c1006db78329be445c66e553966c9de
Tanveerr97 <tanveerkkhan146@gmail.com>
2025-01-05 23:59:27 +0530

    ok

---
+++ Project_1.sql
@@ -0,0 +1,9 @@
+-- Active: 1736099308663@@127.0.0.1@3306@project_1
+CREATE TABLE Books (
+    book_id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    author VARCHAR(255) NOT NULL,
+    genre VARCHAR(100),
+    published_year YEAR,
+    status VARCHAR(20) CHECK(status IN ('Available', 'Checked-out')) DEFAULT 'Available'
+);
