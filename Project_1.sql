f77f164cd431fc5e74b1ed782b57fd0095503415
Tanveerr97 <tanveerkkhan146@gmail.com>
2025-01-06 00:13:56 +0530

    ok

--- Project_1.sql
+++ Project_1.sql
@@ -1,9 +1,9 @@
 -- Active: 1736099308663@@127.0.0.1@3306@project_1
-CREATE TABLE Books (
-    book_id INT AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(255) NOT NULL,
-    author VARCHAR(255) NOT NULL,
-    genre VARCHAR(100),
-    published_year YEAR,
-    status VARCHAR(20) CHECK(status IN ('Available', 'Checked-out')) DEFAULT 'Available'
+CREATE TABLE Members (
+    member_id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    address VARCHAR(255),
+    phone_number VARCHAR(20),
+    email VARCHAR(100),
+    membership_date DATE NOT NULL
 );
