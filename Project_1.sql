6269e83fd4d3520440bdea20bc95d614a6d765f2
Tanveer <tanveerkkhan146@gmail.com>
2025-01-06 14:09:33 +0530

    Update Project_1.sql

--- Project_1.sql
+++ Project_1.sql
@@ -30,20 +30,8 @@ CREATE TABLE Transactions (
     FOREIGN KEY (book_id) REFERENCES Books(book_id),
     FOREIGN KEY (member_id) REFERENCES Members(member_id)
 );
-CREATE TABLE Staff (
-    staff_id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    role VARCHAR(50),
-    contact_number VARCHAR(20),
-    email VARCHAR(100)
-);
-CREATE TABLE Book_Categories (
-    book_id INT,
-    category_id INT,
-    PRIMARY KEY (book_id, category_id),
-    FOREIGN KEY (book_id) REFERENCES Books(book_id),
-    FOREIGN KEY (category_id) REFERENCES Categories(category_id)
-);
+
+
 INSERT INTO Books (title, author, genre, published_year, status)
 VALUES 
     ('The Great Gatsby', 'F. Scott Fitzgerald', 'Fiction', 1925, 'Available'),
@@ -54,29 +42,21 @@ INSERT INTO Members (name, address, phone_number, email, membership_date)
 VALUES
     ('John Doe', '123 Elm Street', '555-1234', 'john.doe@email.com', '2025-01-01'),
     ('Jane Smith', '456 Oak Avenue', '555-5678', 'jane.smith@email.com', '2025-01-02');
-INSERT INTO Staff (name, role, contact_number, email)
-VALUES
-    ('Alice Green', 'Librarian', '555-8765', 'alice.green@email.com'),
-    ('Bob Brown', 'Assistant', '555-4321', 'bob.brown@email.com');
-INSERT INTO Categories (name)
-VALUES
-    ('Fiction'),
-    ('Dystopian'),
-    ('Classics');
-INSERT INTO Book_Categories (book_id, category_id)
-VALUES
-    (1, 1), (2, 2), (3, 1), (4, 1);
+# book available or not
 SELECT title, author, genre, published_year
 FROM Books
 WHERE status = 'Available';
-
+#book out
 INSERT INTO Transactions (book_id, member_id, check_out_date, due_date)
 VALUES (1, 1, '2025-01-05', '2025-01-20');
 UPDATE Books SET status = 'Checked-out' WHERE book_id = 1;
+    #book in
 UPDATE Transactions
 SET check_in_date = '2025-01-22', fine_amount = DATEDIFF(CURRENT_DATE, due_date) * 2
 WHERE transaction_id = 1;
+    
 UPDATE Books SET status = 'Available' WHERE book_id = 1;
+    #over due book
 
 SELECT b.title, m.name, t.due_date, DATEDIFF(CURRENT_DATE, t.due_date) AS overdue_days
 FROM Transactions t
@@ -84,16 +64,8 @@ JOIN Books b ON t.book_id = b.book_id
 JOIN Members m ON t.member_id = m.member_id
 WHERE t.check_in_date IS NULL AND t.due_date < CURRENT_DATE;
 
-SELECT b.title, b.author, c.name AS category
-FROM Books b
-JOIN Book_Categories bc ON b.book_id = bc.book_id
-JOIN Categories c ON bc.category_id = c.category_id
-WHERE c.name = 'Fiction';
-SELECT b.title, t.check_out_date, t.due_date, t.check_in_date, t.fine_amount
-FROM Transactions t
-JOIN Books b ON t.book_id = b.book_id
-WHERE t.member_id = 1;
-SELECT title, author, status FROM Books;
+;
+    # overdue+fine
 SELECT b.title, m.name, t.due_date, DATEDIFF(CURRENT_DATE, t.due_date) AS overdue_days, t.fine_amount
 FROM Transactions t
 JOIN Books b ON t.book_id = b.book_id
